b5123de828cd8460f96409a495a61afc198fc6e2
viviandosreis <40150927+viviandosreis@users.noreply.github.com>
2021-05-15 10:24:27 -0300

    Tópico 03
    Tópico 03

---
+++ topico03.R
@@ -0,0 +1,18 @@
+#-------------- Topico 03-------------
+#aula 04
+
+?iris
+iris$Sepal.Length
+
+#Levels: setosa versicolor virginica -> fator
+#--Species: fator
+iris$Species
+str(iris$Species)
+
+#sumario: calculo das 5 estatisticas sumarias de todas as variaveis do banco
+summary(iris)
+
+#str: estrutura do banco
+str(iris)
+
+
